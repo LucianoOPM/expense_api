554f6c051fd25b1bdf6a14ee7cfce834b5ec2643
Luciano Perez <jesus-61914@hotmail.com>
2024-12-06 23:16:48 -0600

    Update main.ts
    Se agregó la configuración de CORS

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,19 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { ConfigService } from '@nestjs/config';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  await app.listen(3000);
+  const configService = app.get(ConfigService);
+  const port = configService.get('port') as number;
+  const frontUrl = configService.get('frontUrl') as string;
+
+  app.enableCors({
+    origin: [frontUrl],
+    methods: ['GET', 'POST', 'PUT', 'DELETE', 'PATCH'],
+    allowedHeaders: ['Content-Type', 'Authorization'],
+  });
+
+  await app.listen(port);
 }
 bootstrap();
