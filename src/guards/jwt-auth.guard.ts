5ca1e85f6ac92add071106d68a00dc29ccad494b
Luciano Perez <jesus-61914@hotmail.com>
2024-12-23 22:37:49 -0600

    Create jwt-auth.guard.ts
    Added public guard

---
+++ src/guards/jwt-auth.guard.ts
@@ -0,0 +1,25 @@
+import { IS_PUBLIC_KEY } from '@/secure/metaData';
+import { ExecutionContext, Injectable } from '@nestjs/common';
+import { Reflector } from '@nestjs/core';
+import { AuthGuard } from '@nestjs/passport';
+import { Observable } from 'rxjs';
+
+@Injectable()
+export class JwtAuthGuard extends AuthGuard('jwt') {
+  constructor(private readonly reflector: Reflector) {
+    super();
+  }
+  canActivate(
+    context: ExecutionContext,
+  ): boolean | Promise<boolean> | Observable<boolean> {
+    const isPublic = this.reflector.getAllAndOverride<boolean>(IS_PUBLIC_KEY, [
+      context.getHandler(),
+      context.getClass(),
+    ]);
+    if (isPublic) {
+      return true;
+    }
+    return super.canActivate(context);
+  }
+  //si se maneja con roles, se puede implementar el método canActivate y validar el rol
+}
